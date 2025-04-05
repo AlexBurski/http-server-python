3516fb45a949c300c805b325f16294e4627295a5
Alexander Burski <bssb.sbbs@mail.ru>
2025-04-05 19:34:04 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -12,10 +12,16 @@ def main():
     lines = received_request.split("\r\n")
     first_line = lines[0]
     method, path, version = first_line.split(" ")
-    # print(path)
+    # print(lines)
+    headers = {}
+    for header_part in lines[1:]:
+        if header_part == "":
+            break
+        k, v = header_part.split(":", 1)
+        headers[k.strip().lower()] = v.strip()
 
     if path == "/":
-        response= "HTTP/1.1 200 OK\r\n\r\n"
+        response = "HTTP/1.1 200 OK\r\n\r\n"
     elif path.startswith("/echo/"):
         content = path[6:]
         content_length = len(content)
@@ -27,11 +33,24 @@ def main():
             "\r\n"  # End of headers
             f"{content}"
         )
+    elif path.startswith("/user-agent"):
+        content = headers.get("user-agent")
+        content_length = len(content)
+
+        response = (
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: text/plain\r\n"
+            f"Content-Length: {content_length}\r\n"
+            "\r\n"  # End of headers
+            f"{content}"
+        )
+
     else:
         response = "HTTP/1.1 404 Not Found\r\n\r\n"
 
     conn.sendall(response.encode())
     conn.close()
+
 
 if __name__ == "__main__":
     main()
